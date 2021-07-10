d0de66eac0da6d580394cf4ddde184ea25d304e1
Ian Bygrave <ian@bygrave.me.uk>
2021-07-10 13:58:20 +0100

    Add conversion to continue fraction

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,21 @@
+from fractions import Fraction
+from math import floor
+
+
 def cfrac_to_fraction(cfrac):
-    pass
+    ans = Fraction(cfrac.pop(0))
+    if cfrac:
+        ans = ans + (Fraction(1)/cfrac_to_fraction(cfrac))
+    return ans
+
+
+def fraction_to_cfrac(frac):
+    ans = []
+    while True:
+        whole = floor(frac)
+        ans.append(whole)
+        frac = frac - whole
+        if frac == 0:
+            break
+        frac = Fraction(1) / frac
+    return ans
--- test/test_cfrac.py
+++ test/test_cfrac.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 
-from cfrac import cfrac_to_fraction
+from cfrac import cfrac_to_fraction, fraction_to_cfrac
 import pytest
 
 
@@ -9,5 +9,14 @@ import pytest
     ([1,1], Fraction("2")),
     ([1,2,3], Fraction("10/7")),
     ])
-def test_cfrac_to_frac(cfrac, expected_frac):
+def test_cfrac_to_fraction(cfrac, expected_frac):
     assert cfrac_to_fraction(cfrac) == expected_frac
+
+
+@pytest.mark.parametrize("frac,expected_cfrac", [
+    (Fraction("5"), [5]),
+    (Fraction("3/2"), [1,2]),
+    (Fraction("10/7"), [1,2,3]),
+    ])
+def test_fraction_to_cfrac(frac, expected_cfrac):
+    assert fraction_to_cfrac(frac) == expected_cfrac
