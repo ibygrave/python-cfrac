090b16ea99076688f841670a8b78311be99862e2
Ian Bygrave <ian@bygrave.me.uk>
2021-07-10 19:32:19 +0100

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ import setuptools
 setuptools.setup(
     name="cfrac",
     version="1.0",
-    description="Continue Fractions",
+    description="Continued Fractions",
     author="Ian Bygrave",
     author_email="ian@bygrave.me.uk",
     packages=["cfrac"],
-    install_requires=[],
-    entry_points={"console_scripts": []},
 )
