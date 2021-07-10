82e410703335a80be916bf640bfcb7cf845ed958
Ian Bygrave <ian@bygrave.me.uk>
2021-07-10 17:46:09 +0100

    Add limit_denom to conversion back to Fraction

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,15 @@ from fractions import Fraction
 from math import floor
 
 
-def cfrac_to_fraction(cfrac):
-    ans = Fraction(cfrac.pop(0))
-    if cfrac:
-        ans = ans + (Fraction(1)/cfrac_to_fraction(cfrac))
+def cfrac_to_fraction(cfrac, limit_denom=None):
+    wcfrac = cfrac[:]
+    assert limit_denom is None or limit_denom >= 1
+    ans = Fraction(wcfrac.pop(0))
+    if wcfrac:
+        ans = ans + (Fraction(1)/cfrac_to_fraction(wcfrac))
+    if limit_denom is not None and ans.denominator > limit_denom:
+        if len(cfrac) > 1:
+            return cfrac_to_fraction(cfrac[:-1], limit_denom)
     return ans
 
 
--- test/test_cfrac.py
+++ test/test_cfrac.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+import math
 
 from cfrac import cfrac_to_fraction, fraction_to_cfrac
 import pytest
@@ -20,3 +21,17 @@ def test_cfrac_to_fraction(cfrac, expected_frac):
     ])
 def test_fraction_to_cfrac(frac, expected_cfrac):
     assert fraction_to_cfrac(frac) == expected_cfrac
+
+
+CPI = fraction_to_cfrac(Fraction(math.pi))
+
+
+@pytest.mark.parametrize("cfrac,limit_denom,expected_frac", [
+    (CPI, 1, Fraction("3")),
+    (CPI, 20, Fraction("22/7")),
+    (CPI, 100, Fraction("22/7")),
+    (CPI, 2000, Fraction("355/113")),
+    (CPI, 10000, Fraction("355/113")),
+    ])
+def test_cfrac_to_fraction_limit_denom(cfrac, limit_denom, expected_frac):
+    assert cfrac_to_fraction(cfrac, limit_denom=limit_denom) == expected_frac
